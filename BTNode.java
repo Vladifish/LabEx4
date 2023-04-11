e4d2949cdf10fc6a5014cfdb3f41d5709cc78d83
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-04-11 19:02:42 +0800

    ADDED Node constructors

--- BTNode.java
+++ BTNode.java
@@ -5,3 +5,23 @@
  * Lab: Lab Exercise 4
  * File: Binary Tree Node
  */
+
+class BTNode<T> {
+    T info;
+    int level;
+    BTNode<T> left;
+    BTNode<T> right;
+
+    public BTNode(T info) {
+        this.info = info;
+        left = null;
+        right = null;
+    }
+
+    // set null as the parameter for the right/left node if there's just one child
+    public BTNode(T info, BTNode<T> left, BTNode<T> right) {
+        this.info = info;
+        this.left = left;
+        this.right = right;
+    }
+}
