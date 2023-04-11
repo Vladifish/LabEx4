05a6f33f3b4d633ebbf19873883fe8d30e73bb5e
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-04-11 18:49:32 +0800

    added 5 line comment at top

--- BT.java
+++ BT.java
@@ -1,3 +1,10 @@
+/*
+ * Authors: Aaron Jetro A. Alvarez & Vladimir Gray P. Velazco
+ * Section: 1-CSC
+ * Course: ICS-2605
+ * Lab: Lab Exercise 4
+ * File: Binary Tree
+ */
 public class BT {
 
 }
---
+++ BTNode.java
@@ -0,0 +1,7 @@
+/*
+ * Authors: Aaron Jetro A. Alvarez & Vladimir Gray P. Velazco
+ * Section: 1-CSC
+ * Course: ICS-2605
+ * Lab: Lab Exercise 4
+ * File: Binary Tree
+ */
