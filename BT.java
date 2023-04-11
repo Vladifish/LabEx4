e2c0e95dcec7b3003172a26494afb76d9af834d9
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-04-11 19:18:31 +0800

    ADDED Constructor and setRoot

--- BT.java
+++ BT.java
@@ -5,6 +5,21 @@
  * Lab: Lab Exercise 4
  * File: Binary Tree
  */
-public class BT {
+public class BT<T> {
+    BTNode<T> root;
+    int height;
 
+    BT() {
+        root = null;
+        // the height of an empty tree is still 0
+    }
+
+    public void setRoot(BTNode<T> n) {
+        root = n;
+    }
+
+    public static void main(String[] args) {
+        BT<Integer> a = new BT<>();
+        System.out.println(a.height);
+    }
 }
