ed19824162968ee98b65ab6169c5122e34d0c999
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-04-16 18:57:11 +0800

    FIXED: Comment, ADDED: BST Template

--- BST.java
+++ BST.java
@@ -1,3 +1,17 @@
+/*
+ * Authors: Aaron Jetro C. Alvarez & Vladimir Gray P. Velazco
+ * Section: 1-CSC
+ * Course: ICS-2605
+ * Lab: Lab Exercise 4
+ * File: BST.java
+ */
 public class BST<T> extends BT<T> {
 
+    // Templates, so I can work on main
+    void insert() {
+    }
+
+    T search() {
+        return null;
+    }
 }
